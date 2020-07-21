a86df5e335f9f3d24e57829700e864162155629d
Suneel023 <mariochill@yahoo.com>
2020-07-21 21:26:21 +0530

    fetching data from backend

---
+++ Pro03/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def main(request):
+    return HttpResponse("Main Page of Django")
+
+def home(request):
+    return render(request,"first.html")
+
+def second(request):
+    return render(request,"webpages/second.html")
+
+
+def third(request):
+    return render(request,"webpages/third.html", context={'data':"Sunil",'name':"Kumarappa"})
+
+
+def fourth(request):
+    fruits = ["mango","kiwi","orange","banana","apple"]
+    return render(request,"webpages/fourth.html",{'fruits':fruits})
+
+
+def fifth(request):
+    return render(request,"webpages/fifth.html",{'a':10,'b':5})
